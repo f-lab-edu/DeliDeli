a6ab5dd1372277455f2809f14df56978084b3545
jeongeun <sje7543@naver.com>
2021-06-07 00:01:49 +0900

    Create new project

---
+++ src/main/java/flab/delideli/DelideliApplication.java
@@ -0,0 +1,13 @@
+package flab.delideli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelideliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelideliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flab/delideli/DelideliApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.delideli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelideliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
