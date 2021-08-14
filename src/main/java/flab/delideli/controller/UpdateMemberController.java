25ea78c0acd1b8c1c5f5cc2b528f255ad8e29d84
Suyeonkim07 <ksy4836@gmail.com>
2021-08-14 19:37:45 +0900

    url수정&피드백 반영

--- src/main/java/flab/delideli/controller/UpdateMemberController.java
+++ src/main/java/flab/delideli/controller/UpdateMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,27 +22,27 @@ public class UpdateMemberController {
 
     private MemberService memberService;
 
-    private static final ResponseEntity okResponseEntity = new ResponseEntity(HttpStatus.OK);
-    private static final ResponseEntity unauthorizedResponseEntity = new ResponseEntity(HttpStatus.UNAUTHORIZED);
+    private static final ResponseEntity OK_Response_Entity = new ResponseEntity(HttpStatus.OK);
+    private static final ResponseEntity UNAUTHORIZED_ResponseEntity = new ResponseEntity(HttpStatus.UNAUTHORIZED);
     private static final String USER_ID = "USER_ID";
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public ResponseEntity updateUser(HttpSession session, @RequestBody UpdateDTO updateDTO) {
         String currentUserId = (String) session.getAttribute(USER_ID);
         if (currentUserId == null) {
-            return unauthorizedResponseEntity;
+            return UNAUTHORIZED_ResponseEntity;
         }
         memberService.updateUserInfo(currentUserId ,updateDTO);
-        return okResponseEntity;
+        return OK_Response_Entity;
     }
 
-    @RequestMapping(value="/delete", method = RequestMethod.POST)
+    @RequestMapping(value="/{userid}/delete", method = RequestMethod.DELETE)
     public ResponseEntity deleteUser(HttpSession session) {
         String currentUserId = (String) session.getAttribute(USER_ID);
         if (currentUserId == null)
-            return unauthorizedResponseEntity;
+            return UNAUTHORIZED_ResponseEntity;
         memberService.deleteUserInfo(currentUserId);
         session.invalidate();
-        return okResponseEntity;
+        return OK_Response_Entity;
     }
 }
--- src/test/java/flab/delideli/mapper/MemberMapperTest.java
+++ src/test/java/flab/delideli/mapper/MemberMapperTest.java
@@ -17,8 +17,8 @@ public class MemberMapperTest {
     private MemberService memberservice;
     private Encrypt encrypt;
 
-    MemberDTO member1 = new MemberDTO("ddddd", "dda", "ddddda", "0101", "sdad");
-    MemberDTO member2 = new MemberDTO("ddd", "dddda", "ddddda", "0101", "sdad");
+    MemberDTO member1 = new MemberDTO("suykim", "suykima", "김나연", "010-2222-2222", "seong");
+    MemberDTO member2 = new MemberDTO("jeong", "jenoga", "김연아", "010-7777-7777", "seoul");
 
     @Test
     public void memberjoin() throws Exception {
@@ -37,11 +37,11 @@ public class MemberMapperTest {
     @Test
     public void update() throws Exception {
         UpdateDTO updateDTO1 = new UpdateDTO("","","");
-        memberservice.updateUserInfo("ddaad",updateDTO1);
+        memberservice.updateUserInfo("jeong",updateDTO1);
     }
 
     @Test
     public void delete() throws Exception {
-        memberservice.deleteUserInfo("ddaaddddd");
+        memberservice.deleteUserInfo("suykim");
     }
 }
