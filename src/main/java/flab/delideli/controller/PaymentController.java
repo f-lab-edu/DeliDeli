6f67b11352ede5d01b0cfabe9439da9e55dee05a
jeongeun <sje7543@naver.com>
2022-04-17 17:12:44 +0900

    Get Payment Summary

--- src/main/java/flab/delideli/controller/PaymentController.java
+++ src/main/java/flab/delideli/controller/PaymentController.java
@@ -1,14 +1,19 @@
 package flab.delideli.controller;
 
 import flab.delideli.annotation.CurrentUser;
+import flab.delideli.dto.PaymentDTO;
 import flab.delideli.dto.RequestPaymentDTO;
+import flab.delideli.service.payment.CommonPaymentService;
 import flab.delideli.service.payment.PaymentFactory;
 import flab.delideli.service.payment.PaymentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
 	private final PaymentFactory paymentFactory;
+	private final CommonPaymentService commonPaymentService;
 
 	@PostMapping("/{orderId}")
 	@ResponseStatus(HttpStatus.CREATED)
@@ -33,6 +39,19 @@ public class PaymentController {
 			.getType(requestPaymentDTO.getPaymentType());
 
 		paymentService.pay(orderId, userId, requestPaymentDTO);
+	}
+
+	@GetMapping("/{paymentId}")
+	@ApiOperation(value = "한 주문에 대한 결제내역 조회")
+	public PaymentDTO getPaymentSummary(@PathVariable("paymentId") @NotNull long paymentId,
+		@CurrentUser String userId) {
+		return commonPaymentService.getPaymentSummary(paymentId, userId);
+	}
+
+	@GetMapping
+	@ApiOperation(value = "한 회원의 결제내역들 불러오기")
+	public List<Long> getAllPaymentSummariesOfUser(@CurrentUser String userId) {
+		return commonPaymentService.getAllPaymentSummariesOfUser(userId);
 	}
 
 }
---
+++ src/main/java/flab/delideli/service/payment/CommonPaymentService.java
@@ -0,0 +1,23 @@
+package flab.delideli.service.payment;
+
+import flab.delideli.dao.PaymentDao;
+import flab.delideli.dto.PaymentDTO;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CommonPaymentService {
+
+	private final PaymentDao paymentDao;
+
+	public PaymentDTO getPaymentSummary(long paymentId, String userId) {
+		return paymentDao.selectPaymentSummary(paymentId, userId);
+	}
+
+	public List<Long> getAllPaymentSummariesOfUser(String userId) {
+		return paymentDao.selectAllPaymentSummariesOfUser(userId);
+	}
+
+}
