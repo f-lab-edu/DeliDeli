57708c9453422d3d764c5f43fcf4d6f7823c1dcd
김수연 <kimsuyeon@gimsuyeons-MacBook-Air.local>
2022-05-20 20:43:37 +0900

    로직 수정

--- src/main/java/flab/delideli/service/OrderOwnerService.java
+++ src/main/java/flab/delideli/service/OrderOwnerService.java
@@ -5,6 +5,7 @@ import flab.delideli.dao.ShopDao;
 import flab.delideli.dto.OrderDTO;
 import flab.delideli.enums.OrderStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,28 +15,34 @@ public class OrderOwnerService implements OwnerService {
 	private final OwnerDao ownerDao;
 	private final ShopDao shopDao;
 
-	/* 주문완료인 경우에만 조리시작, 주문취소로 변경할 수 있다*/
 	@Override
 	public void updateOrderStatusCooking(long orderId, String userId) {
 		validateCorrectOwnerShop(userId, orderId);
-		ownerDao.updateOrderStatusCooking(orderId);
+		OrderDTO orderDTO = getOrderDTO(orderId);
+		if (orderDTO.getOrderStatus() == OrderStatus.ORDER_COMPLETE) {
+			ownerDao.updateOrderStatusCooking(orderId);
+		}
+		throw new IllegalStateException("조리시작될 수 없는 주문입니다.");
 	}
 
 	@Override
 	public void updateOrderStatusCancel(long orderId, String userId) {
 		validateCorrectOwnerShop(userId, orderId);
-		ownerDao.updateOrderStatusCancel(orderId);
+		OrderDTO orderDTO = getOrderDTO(orderId);
+		if(orderDTO.getOrderStatus() == OrderStatus.ORDER_COMPLETE) {
+			ownerDao.updateOrderStatusCancel(orderId);
+		}
+		throw new IllegalStateException("주문취소할 수 없는 주문입니다.");
 	}
 
-	/* 조리시작인 경우에만 조리완료로 변경할 수 있다*/
 	@Override
 	public void updateOrderStatusCookingComplete(long orderId, String userId) {
 		validateCorrectOwnerShop(userId, orderId);
 		OrderDTO orderDTO = getOrderDTO(orderId);
-		if (orderDTO.getOrderStatus() != OrderStatus.START_COOKING) {
-			throw new IllegalStateException("조리완료가 될 수 없는 주문입니다");
+		if (orderDTO.getOrderStatus() == OrderStatus.START_COOKING) {
+			ownerDao.updateOrderStatusCookingComplete(orderId);
 		}
-		ownerDao.updateOrderStatusCookingComplete(orderId);
+		throw new IllegalStateException("조리완료가 될 수 없는 주문입니다");
 	}
 
 	/*주문이 들어온 곳이 사장님 가게의 주문이 맞는지 확인해야한다*/
